1b29bf9b9476f8573c484e40d1d89898b44796ce
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-07-04 17:32:50 +0530

    Show the board

--- TicTacToe Game/src/com/workshop2/TicTacToe.java
+++ TicTacToe Game/src/com/workshop2/TicTacToe.java
@@ -7,34 +7,45 @@ public class TicTacToe {
     public static char USER;
 
     public static char[] board = new char[10];
-    public static void assignEmptySpace(){
-        char c=' ';
-        for (char i=0;i<board.length;i++){
-            board[i]=c;
+
+    public static void assignEmptySpace() {
+        char c = ' ';
+        for (char i = 0; i < board.length; i++) {
+            board[i] = c;
             System.out.println(board[i]);
         }
     }
-    public static void chooseALetter(){
-        char letter1='X';
-        char letter2='O';
+
+    public static void chooseALetter() {
+        char letter1 = 'X';
+        char letter2 = 'O';
 
         Random r = new Random();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter X or O: ");
-        USER=sc.nextLine().charAt(0);
+        USER = sc.nextLine().charAt(0);
 
-        if (USER==letter1){
-            System.out.println("User has choose "+letter1);
+        if (USER == letter1) {
+            System.out.println("User has choose " + letter1);
             System.out.println("Select the Index where you want to insert");
 
-        } else if (USER==letter2) {
-            System.out.println("User has choose "+letter2);
+        } else if (USER == letter2) {
+            System.out.println("User has choose " + letter2);
             System.out.println("Select the Index where you want to insert");
 
-        }else {
+        } else {
             System.out.println("Invaliad output try again");
         }
 
     }
 
+    public static void showBoard() {
+        System.out.println("\nCurrent Board:");
+        for (char i = 0; i < board.length - 7; i++) {
+            for (char j = 0; j < board.length - 7; j++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+    }
 }
