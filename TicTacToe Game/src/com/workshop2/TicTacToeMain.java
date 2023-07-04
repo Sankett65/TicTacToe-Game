d31b476c3baa7ecfa810de47002c3c14fec8ce7b
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-07-04 17:28:33 +0530

    Assign empty space

--- TicTacToe Game/src/com/workshop2/TicTacToeMain.java
+++ TicTacToe Game/src/com/workshop2/TicTacToeMain.java
@@ -3,5 +3,6 @@ package com.workshop2;
 public class TicTacToeMain {
     public static void main(String[] args) {
         System.out.println("Welcome to the  TicTacToe Game");
+        TicTacToe.assignEmptySpace();
     }
 }
